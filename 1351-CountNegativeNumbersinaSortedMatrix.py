4772a5bd43cd372f8ce224762129c69cf0dd8396
PasunoorNikhil <43120288+PasunoorNikhil@users.noreply.github.com>
2020-03-19 19:20:36 -0400

    Create 1351-CountNegativeNumbersinaSortedMatrix.py

---
+++ 1351-CountNegativeNumbersinaSortedMatrix.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        count=0
+        for i in grid:
+            for j in reversed(i):
+                if j<0:
+                    count+=1
+                else:
+                    break
+        return count
+                
