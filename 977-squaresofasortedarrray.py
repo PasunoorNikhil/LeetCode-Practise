af4b879ba0b4665a62074f693efe2649bffcd0a3
PasunoorNikhil <43120288+PasunoorNikhil@users.noreply.github.com>
2020-04-01 19:05:29 -0400

    Create 977-squaresofasortedarrray.py

---
+++ 977-squaresofasortedarrray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        result=[]
+        for x in A:
+            z=x*x
+            result.append(z)
+        result.sort()
+        return result
