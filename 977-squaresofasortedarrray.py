55e8a426c7bc27ed31c6dbf208a177ed5810d461
PasunoorNikhil <43120288+PasunoorNikhil@users.noreply.github.com>
2020-06-03 15:13:40 -0400

    Delete 977-squaresofasortedarrray.py

--- 977-squaresofasortedarrray.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def sortedSquares(self, A: List[int]) -> List[int]:
-        result=[]
-        for x in A:
-            z=x*x
-            result.append(z)
-        result.sort()
-        return result
